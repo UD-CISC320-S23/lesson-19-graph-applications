833cc02118cf5e6b157a786bfb0c88f916a5ae03
LeonGIA <lonphi12@gmail.com>
2023-04-16 15:24:05 -0400

    Removed duplicate entries in the raw data for the graph. Adjusted the graph-creation function so that it works with a dictionary where each key is a node and each value is the node's parents. Adjusted the problem so that it is more in line with what BFS can do. Adjusted the algorithm so that it can now find all the children of a given node.

--- BFS_Math.py
+++ BFS_Math.py
@@ -1,120 +1,67 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-adjacency_dict = {"MATH 010" : [],
-"MATH 114" : ['MATH 010'],
-"MATH 115" : ['MATH 010'],
-"MATH 221" : ['MATH 115', 'MATH 117'],
-"MATH 222" : ['MATH 221', 'MATH 241'],
-"MATH 230" : ['MATH 221'],
-"MATH 205" : ['MATH 210', 'MATH 230'],
-"MATH 349" : ['MATH 230', 'MATH 242'],
-"MATH 302" : ['MATH 242', 'MATH 349', 'MATH 351'],
-"MATH 420" : ['MATH 302', 'MATH 349', 'MATH 350'],
-"MATH 419" : ['MATH 219', 'MATH 243', 'MATH 349', 'MATH 350'],
-"MATH 420" : ['MATH 302', 'MATH 349', 'MATH 350'],
-"MATH 426" : ['MATH 305', 'MATH 351', 'MATH 349'],
-"MATH 428" : ['MATH 426', 'MATH 353'],
-"MATH 451" : ['MATH 349', 'MATH 245'],
-"MATH 308" : ['MATH 221', 'MATH 232', 'MATH 241'],
-"MATH 117" : ['MATH 010'],
-"MATH 221" : ['MATH 115', 'MATH 117'],
-"MATH 222" : ['MATH 221', 'MATH 241'],
-"MATH 230" : ['MATH 221'],
-"MATH 205" : ['MATH 210', 'MATH 230'],
-"MATH 349" : ['MATH 230', 'MATH 242'],
-"MATH 302" : ['MATH 242', 'MATH 349', 'MATH 351'],
-"MATH 420" : ['MATH 302', 'MATH 349', 'MATH 350'],
-"MATH 419" : ['MATH 219', 'MATH 243', 'MATH 349', 'MATH 350'],
-"MATH 420" : ['MATH 302', 'MATH 349', 'MATH 350'],
-"MATH 426" : ['MATH 305', 'MATH 351', 'MATH 349'],
-"MATH 428" : ['MATH 426', 'MATH 353'],
-"MATH 451" : ['MATH 349', 'MATH 245'],
-"MATH 308" : ['MATH 221', 'MATH 232', 'MATH 241'],
-"MATH 241" : ['MATH 117'],
-"MATH 219" : ['MATH 241'],
-"MATH 419" : ['MATH 219', 'MATH 243', 'MATH 349', 'MATH 350'],
-"MATH 222" : ['MATH 221', 'MATH 241'],
-"MATH 242" : ['MATH 241', 'MATH 232'],
-"MATH 243" : ['MATH 242'],
-"MATH 379" : ['MATH 243'],
-"MATH 380" : ['MATH 379'],
-"MATH 382" : ['MATH 380'],
-"MATH 419" : ['MATH 219', 'MATH 243', 'MATH 349', 'MATH 350'],
-"MATH 245" : ['MATH 210', 'MATH 242'],
-"MATH 401" : ['MATH 245'],
-"MATH 451" : ['MATH 349', 'MATH 245'],
-"MATH 302" : ['MATH 242', 'MATH 349', 'MATH 351'],
-"MATH 420" : ['MATH 302', 'MATH 349', 'MATH 350'],
-"MATH 305" : ['MATH 242'],
-"MATH 426" : ['MATH 305', 'MATH 351', 'MATH 349'],
-"MATH 428" : ['MATH 426', 'MATH 353'],
-"MATH 349" : ['MATH 230', 'MATH 242'],
-"MATH 302" : ['MATH 242', 'MATH 349', 'MATH 351'],
-"MATH 420" : ['MATH 302', 'MATH 349', 'MATH 350'],
-"MATH 419" : ['MATH 219', 'MATH 243', 'MATH 349', 'MATH 350'],
-"MATH 420" : ['MATH 302', 'MATH 349', 'MATH 350'],
-"MATH 426" : ['MATH 305', 'MATH 351', 'MATH 349'],
-"MATH 428" : ['MATH 426', 'MATH 353'],
-"MATH 451" : ['MATH 349', 'MATH 245'],
-"MATH 308" : ['MATH 221', 'MATH 232', 'MATH 241'],
-"MATH 231" : ['MATH 010'],
-"MATH 232" : ['MATH 231'],
-"MATH 242" : ['MATH 241', 'MATH 232'],
-"MATH 243" : ['MATH 242'],
-"MATH 379" : ['MATH 243'],
-"MATH 380" : ['MATH 379'],
-"MATH 382" : ['MATH 380'],
-"MATH 419" : ['MATH 219', 'MATH 243', 'MATH 349', 'MATH 350'],
-"MATH 245" : ['MATH 210', 'MATH 242'],
-"MATH 401" : ['MATH 245'],
-"MATH 451" : ['MATH 349', 'MATH 245'],
-"MATH 302" : ['MATH 242', 'MATH 349', 'MATH 351'],
-"MATH 420" : ['MATH 302', 'MATH 349', 'MATH 350'],
-"MATH 305" : ['MATH 242'],
-"MATH 426" : ['MATH 305', 'MATH 351', 'MATH 349'],
-"MATH 428" : ['MATH 426', 'MATH 353'],
-"MATH 349" : ['MATH 230', 'MATH 242'],
-"MATH 302" : ['MATH 242', 'MATH 349', 'MATH 351'],
-"MATH 420" : ['MATH 302', 'MATH 349', 'MATH 350'],
-"MATH 419" : ['MATH 219', 'MATH 243', 'MATH 349', 'MATH 350'],
-"MATH 420" : ['MATH 302', 'MATH 349', 'MATH 350'],
-"MATH 426" : ['MATH 305', 'MATH 351', 'MATH 349'],
-"MATH 428" : ['MATH 426', 'MATH 353'],
-"MATH 451" : ['MATH 349', 'MATH 245'],
-"MATH 308" : ['MATH 221', 'MATH 232', 'MATH 241'] }
+def build_graph_from_dict(parents_dict):
+    # Create an empty directed graph
+    graph = nx.DiGraph()
 
-# create an empty graph
-G = nx.Graph()
+    # Add all nodes to the graph
+    nodes = parents_dict.keys()
+    graph.add_nodes_from(nodes)
 
-# loop through each key in the dictionary
-for node in adjacency_dict.keys():
+    # Add edges between nodes and their parents
+    for node, parents in parents_dict.items():
+        for parent in parents:
+            graph.add_edge(parent, node)
 
-    # add the node to the graph
-    G.add_node(node)
+    return graph
 
-    # loop through each of its adjacent nodes
-    for adjacent_node in adjacency_dict[node]:
-
-        # add the edge between the node and the adjacent node
-        G.add_edge(node, adjacent_node)
-
-def bfs_path(graph, start, end):
-    # Use breadth-first search to generate a list of nodes in the path
-    bfs_tree = nx.bfs_tree(graph, start)
-    path = [end]
-    node = end
-    while node != start:
-        predecessors = list(bfs_tree.predecessors(node))
-        if not predecessors:
-            return None
-        node = predecessors[0]
-        path.append(node)
+def find_direct_children(graph, source):
+    # Create a BFS tree starting from the source node
+    bfs_tree = nx.bfs_tree(graph, source, depth_limit=1)
     
-    # Reverse the path so that it goes from start to end
-    path.reverse()
+    children = []
+    for node in bfs_tree:
+        if node != source:
+            children.append(node)
     
-    return path
+    if not children:
+        return "There are no other courses that have the one you entered as a prereq!"
+    return children
 
-path = bfs_path(G, "MATH 010", "MATH 243")
-print(path)
+if __name__ == '__main__':
+    parents_dict = {
+        "MATH 010" : [],
+        "MATH 114" : ['MATH 010'],
+        "MATH 115" : ['MATH 010'],
+        "MATH 221" : ['MATH 115', 'MATH 117'],
+        "MATH 222" : ['MATH 221', 'MATH 241'],
+        "MATH 230" : ['MATH 221'],
+        "MATH 205" : ['MATH 210', 'MATH 230'],
+        "MATH 349" : ['MATH 230', 'MATH 242'],
+        "MATH 302" : ['MATH 242', 'MATH 349', 'MATH 351'],
+        "MATH 420" : ['MATH 302', 'MATH 349', 'MATH 350'],
+        "MATH 419" : ['MATH 219', 'MATH 243', 'MATH 349', 'MATH 350'],
+        "MATH 426" : ['MATH 305', 'MATH 351', 'MATH 349'],
+        "MATH 428" : ['MATH 426', 'MATH 353'],
+        "MATH 451" : ['MATH 349', 'MATH 245'],
+        "MATH 308" : ['MATH 221', 'MATH 232', 'MATH 241'],
+        "MATH 117" : ['MATH 010'],
+        "MATH 241" : ['MATH 117'],
+        "MATH 219" : ['MATH 241'],
+        "MATH 242" : ['MATH 241', 'MATH 232'],
+        "MATH 243" : ['MATH 242'],
+        "MATH 379" : ['MATH 243'],
+        "MATH 380" : ['MATH 379'],
+        "MATH 382" : ['MATH 380'],
+        "MATH 245" : ['MATH 210', 'MATH 242'],
+        "MATH 401" : ['MATH 245'],
+        "MATH 305" : ['MATH 242'],
+        "MATH 231" : ['MATH 010'],
+        "MATH 232" : ['MATH 231'],
+    }
+    G = build_graph_from_dict(parents_dict)
+    
+    inp = input("Please enter a course. You must enter a course ID following the example format: \"MATH 242\"\nThe BFS algorithm will determine what courses you can take once you have completed the course you entered: ")
+    courses = find_direct_children(G, inp)
+    print("Here are the courses you can take now:\n" + str(courses))
