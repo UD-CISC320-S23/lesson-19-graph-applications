4ee04b7bce712c89ad3a7170d771aa6ffa1ebb66
aaron oster <bigaaron@udel.edu>
2023-04-19 10:51:21 -0400

    manned up and created my friendship graph of ancienct sivilizations in order to determine how many cliques ther are

---
+++ .github/DFS_friendship_graph.py
@@ -0,0 +1,45 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.Graph()
+G.add_node("K") #King
+G.add_node("Q") #Queen
+G.add_node("Kn") #Knight
+G.add_node("Peasants")
+G.add_node("G") #Gaurds
+G.add_node("F") #Farmer
+G.add_node("W") #Witch
+G.add_node("Ma") #Magician
+G.add_node("M") #Mathematician
+G.add_node("A") #Astronomer
+G.add_node("S") #Sailor
+G.add_node("Mo") #MonK
+G.add_node("mer") #Merchant
+G.add_node("B") #Botanist
+G.add_node("Dr") #Doctor
+G.add_node("Prank") #Prankster
+G.add_node("d") #drunkard
+G.add_node("Ph") #Philosopher
+G.add_edge("Ph", "M", )
+G.add_edge("M","A")
+G.add_edge("W", "Ma")
+G.add_edge("Dr", "M")
+G.add_edge("Dr", "A")
+G.add_edge("K", "Q")
+G.add_edge("K", "Kn")
+G.add_edge("K", "G")
+G.add_edge("Q", "KN")
+G.add_edge("Q", "G")
+G.add_edge("mer", "F")
+G.add_edge("mer", "S")
+G.add_edge("F", "S")
+G.add_edge("Ma", "W")
+G.add_edge("W", "d")
+G.add_edge("B", "mer")
+G.add_edge("Mo", "M")
+G.add_edge("Mo", "Dr")
+G.add_edge("Ph", "Mo")
+G.add_edge("Prank", "Ma")
+
+nx.draw(G,node_color = "red",with_labels=True, node_size= 300 )
+plt.show()
