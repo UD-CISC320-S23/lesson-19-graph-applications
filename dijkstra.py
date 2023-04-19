af301f194fa02a71d14ef3f012e0bb4da16a1823
Avinash C <avinashc@udel.edu>
2023-04-19 16:48:36 -0400

    Solved dijkstra problem

--- dijkstra.py
+++ dijkstra.py
@@ -1,3 +1,6 @@
 import networkx as nx
+import locationGraph
 
-G = nx.Graph()
+# Find shortest path from Willard Hall to Perkins
+ans_path = nx.dijkstra_path(locationGraph.g, "Willard Hall", "Perkins")
+print(f'To get from Willard to Perkins using the shortest path you need to follow this path:\n\t{ans_path}')
