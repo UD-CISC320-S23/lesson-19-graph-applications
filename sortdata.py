ada7c42f537cbbc17eed5246f4c34942276448b1
christiankuhnn <kuhnc@udel.edu>
2023-04-20 07:45:04 -0400

    Ordered webpages(nodes) added

--- sortdata.py
+++ sortdata.py
@@ -1,19 +1,49 @@
 ####we need a function to sort our data in the json file
+searchtext=","
+replace="\n"
+with open('data.txt','r') as text_file:
+    data=text_file.read()
+    data=data.replace(searchtext,replace)
 
-text = open('data.txt','r')
-
-web_pages = text.read()
-
+with open('data.txt','w') as text_file:
+    text_file.write(data)
 
 ## we need to sort by hierarchy of pages 
-##we can do this by amount of parenthesis in the url
-
-##step 1 remove "checked" 
+##we can do this by amount of slashes in the url
 
 
+main_pages=[]
+secondary_pages=[]
+third=[]
+fourth=[]
+fifth=[]
+with open('data.txt') as text_file:
+    for line in text_file:
+        if line.count("/") ==3:
+            main_pages.append(line)
+        if line.count("/") ==4:
+            secondary_pages.append(line)
+        if line.count("/") ==5:
+            third.append(line)
+        if line.count("/") ==6:
+            fourth.append(line)
+        if line.count("/") ==7:
+            fifth.append(line)
+with open('orderedPages.txt','a') as new_text:
+    for i in main_pages:
+        new_text.write(i)
+        new_text.write("\n")
+    for i in secondary_pages:
+        new_text.write(i)
+        new_text.write("\n")
+    for i in third:
+        new_text.write(i)
+        new_text.write("\n")
+    for i in fourth:
+        new_text.write(i)
+        new_text.write("\n")
 
-##step 2 sort by amount of /  in the url which will give us hierachical order
-##this will give us nodes of our graph
+    
 
 
 
