d16f1310717318382f375e7200b5d69f05256b57
adtrunzo <adtrunzo@udel.edu>
2023-04-22 22:27:24 -0400

    Dijkstra Complete

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,8 +1,7 @@
-#a mathematician is trying to find the shortest path from the  inside a city to the kings castle
-#the king is very nervous the witch may try to poison the towns water supply, and 
-#A notorious witch lives in the town of ____, and she gives King ___ the creeps. He is nervous the Witch may poison the water supply
-#so he asked his most trustworthy Mathematician Chatus Gptus to determine which roads would provide the quickest travel between the 
-#witches hut and the castle, so, should anything fishy occur, his knights could swiftly deliever judgement to the witch. 
+
+#A notorious witch lives in the town of Cisc, and she gives King Algo the creeps. The King is nervous that the Witch may poison the water supply,
+#so he asked his most trustworthy Mathematician, Chatus Gptus to determine which roads would provide the quickest travel between the 
+#castle and the witches hut, allowing the king's knights to swiftly deliever judgement should something suspicious occur. 
 #
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -10,45 +9,97 @@ import matplotlib.pyplot as plt
 
 G = nx.Graph()
 G.add_node("Castle") 
-G.add_node("WitchHut") #Queen
-G.add_node("FlowerShop") #Knight
+G.add_node("WitchHut") 
+G.add_node("FlowerShop") 
 G.add_node("Piazza")
-G.add_node("FarmersMarket") #Gaurds
-G.add_node("MysteryShack") #Farmer
-G.add_node("Well") #Witch
-G.add_node("Guardpost") #Magician
-G.add_node("Observatory") #Mathematician
-G.add_node("Dock") #Astronomer
-G.add_node("Monastary") #Sailor
-G.add_node("TradingPost") #MonK
-G.add_node("Hospital") #Merchant
-G.add_node("Saloon") #Botanist
-G.add_node("Backrooms") #Doctor
-G.add_node("School") #Prankster
-G.add_node("ManorHouse") #drunkard
-G.add_node("Cottage") #Philosopher
+G.add_node("FarmersMarket") 
+G.add_node("MysteryShack") 
+G.add_node("Well") 
+G.add_node("Guardpost1") 
+G.add_node("Guardpost2") 
+G.add_node("FishMarket") 
+G.add_node("Cottage1") 
+G.add_node("Cottage2") 
+G.add_node("Cottage3") 
+G.add_node("Cottage4") 
+G.add_node("Cottage5") 
+G.add_node("Cottage6") 
+G.add_node("Cottage7") 
+G.add_node("Cottage8") 
+G.add_node("Cottage?") 
+G.add_node("Observatory") 
+G.add_node("Dock") 
+G.add_node("Monastary") 
+G.add_node("TradePost") 
+G.add_node("Hospital") 
+G.add_node("Saloon") 
+G.add_node("Backrooms") 
+G.add_node("School") 
+G.add_node("Manor") 
 G.add_node("Stables") 
-G.add_edge("Castle", "Guardpost", weight=1)
-G.add_edge("Guardpost", "Stables", weight=2)
-G.add_edge("Stables", "Saloon", weight=6)
-G.add_edge("Hospital", "Stables", weight=4)
-G.add_edge("Saloon", "Dock", weight=7)
-G.add_edge("Observatory", "Monastary", weight=2)
-G.add_edge("TradingPost", "FarmersMarket", weight=1)
-G.add_edge("WitchHut", "MysteryShack", weight=3)
-G.add_edge("MysteryShack", "Well", weight=15)
-G.add_edge("School", "Well", weight=5)
-G.add_edge("Well", "FarmersMarket", weight=3)
-G.add_edge("FlowerShop", "Well", weight=3)
-G.add_edge("Cottage", "ManorHouse", weight=11)
-G.add_edge("Piazza", "Well", weight=1)
-G.add_edge("FlowerShop", "Piazza", weight=4)
-G.add_edge("School", "Piazza", weight=2)
-G.add_edge("Monastary", "Piazza", weight=3)
-G.add_edge("Hospital", "Saloon", weight=3)
-G.add_edge("Hospital", "School", weight=7)
-G.add_edge("Cottage", "FarmersMarket", weight=3)
-G.add_edge("ManorHouse", "FarmersMarket", weight=3)
+G.add_edge("Castle", "Guardpost1", weight=3)
+G.add_edge("Castle", "Guardpost2", weight=3)
+G.add_edge("Castle", "Manor", weight=4)
+G.add_edge("Manor", "Guardpost1", weight=2)
+G.add_edge("Manor", "Guardpost2", weight=2)
+G.add_edge("Guardpost1", "TradePost", weight=3)
+G.add_edge("Guardpost2", "Stables", weight=1)
+G.add_edge("Guardpost2", "TradePost", weight=3)
+G.add_edge("Manor", "TradePost", weight=3)
+G.add_edge("Stables", "TradePost", weight=1)
+G.add_edge("TradePost", "FarmersMarket", weight=1)
+G.add_edge("FarmersMarket", "FlowerShop", weight=1)
+G.add_edge("FarmersMarket", "FishMarket", weight=1)
+G.add_edge("FishMarket", "Dock", weight=2)
+G.add_edge("Dock", "Saloon", weight=3)
+G.add_edge("FarmersMarket", "Saloon", weight=2)
+G.add_edge("FarmersMarket", "Well", weight=3)
+G.add_edge("Saloon", "Well", weight=3)
+G.add_edge("TradePost", "Well", weight=2)
+G.add_edge("Monastary", "Well", weight=2)
+G.add_edge("Monastary", "School", weight=2)
+G.add_edge("Monastary", "Observatory", weight=6)
+G.add_edge("School", "Cottage1", weight=1)
+G.add_edge("Cottage2", "Cottage1", weight=7)
+G.add_edge("School", "Cottage2", weight=1)
+G.add_edge("Cottage1", "Cottage8", weight=3)
+G.add_edge("Observatory", "Cottage8", weight=2)
+G.add_edge("Monastary", "Cottage7", weight=8)
+G.add_edge("Cottage8", "Cottage7", weight=1)
+G.add_edge("Cottage7", "Cottage3", weight=1)
+G.add_edge("Cottage7", "Cottage5", weight=1)
+G.add_edge("Cottage3", "Well", weight=1)
+G.add_edge("Cottage6", "Cottage7", weight=3)
+G.add_edge("Cottage5", "Cottage6", weight=2)
+G.add_edge("Cottage3", "Cottage4", weight=1)
+G.add_edge("Cottage4", "Saloon", weight=1)
+G.add_edge("Hospital", "Saloon", weight=2)
+G.add_edge("Hospital", "Cottage5", weight=1)
+G.add_edge("Cottage6", "Cottage?", weight=1)
+G.add_edge("Cottage?", "MysteryShack", weight=9)
+G.add_edge("MysteryShack", "WitchHut", weight=5)
+G.add_edge("Cottage6", "Hospital", weight=1)
+G.add_edge("Cottage5", "Cottage3", weight=1)
+G.add_edge("Piazza", "School", weight=1)
+G.add_edge("Piazza", "Observatory", weight=1)
+G.add_edge("Piazza", "Cottage1", weight=1)
+G.add_edge("Piazza", "Monastary", weight=1)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 nx.draw(G,node_color = "green",with_labels=True, node_size= 300 )
 plt.show()
+
+#solution
+print(nx.dijkstra_path(G, "Castle", "WitchHut", weight='weight'))
--- index.md
+++ index.md
@@ -4,7 +4,7 @@
 
 Group Members:
 * First member aoster@udel.edu
-* Second member (email)
+* Second member adtrunzo@udel.edu
 * Third member (email)
 * Fourth member (email)
 
@@ -20,6 +20,7 @@ $> pip install networkx
 
 ```python
 import networkx as nx
+import matplotlib.pyplot as plt
 ```
 
 # First Problem Title
@@ -148,3 +149,131 @@ print(nx.number_connected_components(G))
 
 **Interpretation of Results**:
 This means that there are 5 different connected graphs. This means that there are 5 different social groups and people are disconnected from one another. It is interesting to see how is more connected to each other than other types of people.
+
+
+
+
+
+**Informal Description**: 
+A notorious witch lives in the town of Cisc, and she gives King Algo the creeps. The King is nervous that the Witch may poison the water supply,
+so he asked his most trustworthy Mathematician, Chatus Gptus to determine which roads would provide the quickest travel between the 
+castle and the witches hut, allowing the king's knights to swiftly deliever judgement should something suspicious occur. 
+
+Takes a group of locations with roads that connect them, and determines the shortest path from one location to another
+> **Formal Description**:
+Determine the shortest path between two nodes in a graph. 
+>  * Input: G(V, E): Graph of Vertices and Edges
+>  * Output: List: List of Nodes in the Shortest Path
+
+**Graph Problem/Algorithm**: [SSSP]
+Dijkstra's Algorithm
+
+**Visualization**:
+
+![Image goes here](Shortest_Path.png)
+
+
+**Setup code**:
+
+```python
+
+
+G = nx.Graph()
+G.add_node("Castle") 
+G.add_node("WitchHut") 
+G.add_node("FlowerShop") 
+G.add_node("Piazza")
+G.add_node("FarmersMarket") 
+G.add_node("MysteryShack") 
+G.add_node("Well") 
+G.add_node("Guardpost1") 
+G.add_node("Guardpost2") 
+G.add_node("FishMarket") 
+G.add_node("Cottage1") 
+G.add_node("Cottage2") 
+G.add_node("Cottage3") 
+G.add_node("Cottage4") 
+G.add_node("Cottage5") 
+G.add_node("Cottage6") 
+G.add_node("Cottage7") 
+G.add_node("Cottage8") 
+G.add_node("Cottage?") 
+G.add_node("Observatory") 
+G.add_node("Dock") 
+G.add_node("Monastary") 
+G.add_node("TradePost") 
+G.add_node("Hospital") 
+G.add_node("Saloon") 
+G.add_node("Backrooms") 
+G.add_node("School") 
+G.add_node("Manor") 
+G.add_node("Stables") 
+G.add_edge("Castle", "Guardpost1", weight=3)
+G.add_edge("Castle", "Guardpost2", weight=3)
+G.add_edge("Castle", "Manor", weight=4)
+G.add_edge("Manor", "Guardpost1", weight=2)
+G.add_edge("Manor", "Guardpost2", weight=2)
+G.add_edge("Guardpost1", "TradePost", weight=3)
+G.add_edge("Guardpost2", "Stables", weight=1)
+G.add_edge("Guardpost2", "TradePost", weight=3)
+G.add_edge("Manor", "TradePost", weight=3)
+G.add_edge("Stables", "TradePost", weight=1)
+G.add_edge("TradePost", "FarmersMarket", weight=1)
+G.add_edge("FarmersMarket", "FlowerShop", weight=1)
+G.add_edge("FarmersMarket", "FishMarket", weight=1)
+G.add_edge("FishMarket", "Dock", weight=2)
+G.add_edge("Dock", "Saloon", weight=3)
+G.add_edge("FarmersMarket", "Saloon", weight=2)
+G.add_edge("FarmersMarket", "Well", weight=3)
+G.add_edge("Saloon", "Well", weight=3)
+G.add_edge("TradePost", "Well", weight=2)
+G.add_edge("Monastary", "Well", weight=2)
+G.add_edge("Monastary", "School", weight=2)
+G.add_edge("Monastary", "Observatory", weight=6)
+G.add_edge("School", "Cottage1", weight=1)
+G.add_edge("Cottage2", "Cottage1", weight=7)
+G.add_edge("School", "Cottage2", weight=1)
+G.add_edge("Cottage1", "Cottage8", weight=3)
+G.add_edge("Observatory", "Cottage8", weight=2)
+G.add_edge("Monastary", "Cottage7", weight=8)
+G.add_edge("Cottage8", "Cottage7", weight=1)
+G.add_edge("Cottage7", "Cottage3", weight=1)
+G.add_edge("Cottage7", "Cottage5", weight=1)
+G.add_edge("Cottage3", "Well", weight=1)
+G.add_edge("Cottage6", "Cottage7", weight=3)
+G.add_edge("Cottage5", "Cottage6", weight=2)
+G.add_edge("Cottage3", "Cottage4", weight=1)
+G.add_edge("Cottage4", "Saloon", weight=1)
+G.add_edge("Hospital", "Saloon", weight=2)
+G.add_edge("Hospital", "Cottage5", weight=1)
+G.add_edge("Cottage6", "Cottage?", weight=1)
+G.add_edge("Cottage?", "MysteryShack", weight=9)
+G.add_edge("MysteryShack", "WitchHut", weight=5)
+G.add_edge("Cottage6", "Hospital", weight=1)
+G.add_edge("Cottage5", "Cottage3", weight=1)
+G.add_edge("Piazza", "School", weight=1)
+G.add_edge("Piazza", "Observatory", weight=1)
+G.add_edge("Piazza", "Cottage1", weight=1)
+G.add_edge("Piazza", "Monastary", weight=1)
+
+nx.draw(G,node_color = "green",with_labels=True, node_size= 300 )
+plt.show()
+```
+
+
+
+**Solution code:**
+```python
+print(nx.dijkstra_path(G, "Castle", "WitchHut", weight='weight'))
+
+```
+**Output**
+
+```
+['Castle', 'Guardpost2', 'Stables', 'TradePost', 'Well', 'Cottage3', 'Cottage5', 'Cottage6', 'Cottage?', 'MysteryShack', 'WitchHut']
+```
+
+**Interpretation of Results**:
+This means that the shortest path from the Castle to the WitchHut is Castle->Guardpost2->Stables->TradePost->Well->Cottage3->Cottage5->Cottage6->Cottage?->MysteryShack->WitchHut
+
+
